69ad68121042cde33f2bd3520ffb8915a58daf43
TAR <isip_a.r.taran@mpt.ru>
2023-02-22 12:22:01 +0300

    Добавил новые css, начал делать окно организации

--- electrontest/templates/menu.js
+++ electrontest/templates/menu.js
@@ -2,24 +2,24 @@ var headerText = `<div class="menu" name="nav" id="nav" onmouseout="HideNavigati
         <img class="imgformenu" src="styles/main-menu.png" align="right">
         <nav class="menu-list">
             <div class="menu-div">
-                <a href="#" style="--clr:#39FF14" class="menu-a"><span>Главная</span><i></i></a>
+                <a href="index.html" style="--clr:#39FF14" class="menu-a"><span>Главная</span><i></i></a>
                     <img class="menu-img" src="styles/home.png" align="right">
             </div>
             <div class="menu-div">
-                <a href="#" style="--clr:#39FF14" class="menu-a"><span>Финансы</span><i></i></a>
+                <a href="finances.html" style="--clr:#39FF14" class="menu-a"><span>Финансы</span><i></i></a>
                 <img class="menu-img" src="styles/stats.png" align="right">
             </div>
             <div class="menu-div">
-                <a href="#" style="--clr:#39FF14" class="menu-a"><span>Отделы</span><i></i></a>
+                <a href="department.html" style="--clr:#39FF14" class="menu-a"><span>Отделы</span><i></i></a>
                 <img class="menu-img" src="styles/it-department.png" align="right">
             </div>
             <div class="menu-div">
-                <a href="#" style="--clr:#39FF14" class="menu-a"><span>Сотрудники</span><i></i></a>
+                <a href="employee.html" style="--clr:#39FF14" class="menu-a"><span>Сотрудники</span><i></i></a>
                 <img class="menu-img" src="styles/employees.png" align="right">
             </a>
             </div>
             <div class="menu-div">
-                <a href="#" style="--clr:#39FF14" class="menu-a"><span>Организация</span><i></i></a>
+                <a href="organization.html" style="--clr:#39FF14" class="menu-a"><span>Организация</span><i></i></a>
                 <img class="menu-img" src="styles/strategy.png" align="right"/>
             </div>
         </nav>
--- electrontest/templates/styles.js
+++ electrontest/templates/styles.js
@@ -7,3 +7,41 @@ function HideNavigation() {
     element=document.getElementById("nav")
     element.style.transform="translateX(-80%)"
 }
+
+function PaintItemCombobox(element){
+    element.style.backgroundColor="#cccccc";
+}
+
+function UnPaintItemCombobox(element){
+    element.style.backgroundColor="#ddd";
+}
+
+function HideScrollForNumber() {
+
+    document.addEventListener("wheel", (event) => {
+        if (document.activeElement.type === "number") {
+            document.activeElement.blur();
+        }
+    });
+}
+
+function CheckBoxClick(check) {
+    if (check.checked) {
+        element=document.getElementsByClassName("form-control")
+        for(var i = 0; i < element.length; i++){
+            element[i].disabled=false;
+        }
+    } else {
+        element=document.getElementsByClassName("form-control")
+        for(var i = 0; i < element.length; i++){
+            element[i].disabled=true;
+        }
+    }
+};
+
+element=document.getElementsByClassName("form-control")
+for(var i = 0; i < element.length; i++){
+    element[i].disabled=true;
+}
+
+HideScrollForNumber();
